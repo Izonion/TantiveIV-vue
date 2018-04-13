ccb3456681daf9280f7c655612ba2f77eb14c8b5
Isaac Warner <isaackeet@gmail.com>
2018-04-13 11:34:35 -0600

    Text module has text now.

--- src/components/modules/TextModule.vue
+++ src/components/modules/TextModule.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="module">
-    TEXT
+    <div v-if="editing">
+      <textarea class="text-content" v-model.sync="content"></textarea>
+      <button @click="editing = !editing">Save</button>
+    </div>
+    <div v-else>
+      <p class="text-content" v-text="content"></p>
+      <button @click="editing = !editing">Edit</button>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "TextModule"
+    name: "TextModule",
+    data() {
+      return {
+        editing: false,
+        content: "DEFAULT"
+      }
+    }
   }
 </script>
