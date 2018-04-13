6d51b82a3bcec6ef8c860d02957b54539bd15879
Marissa Angell <littlemissriss@gmail.com>
2018-04-13 10:14:44 -0600

    edited video module

---
+++ src/components/VideoModule.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="module">
+    <iframe width="560" height="315" :src="embed-url" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "VideoModule",
+    data: {
+    	embed-url: "https://www.youtube.com/watch?v=C0DPdy98e4c"
+    }
+  }
+
+</script>
