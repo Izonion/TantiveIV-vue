4dfa222042c2f485d02174dca871da7297f15fc2
Isaac Warner <isaackeet@gmail.com>
2018-04-13 10:35:29 -0600

    Video module works now.

--- src/components/modules/VideoModule.vue
+++ src/components/modules/VideoModule.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="module">
-    <iframe width="560" height="315" :src="embed-url" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <iframe width="560" height="315" :src="embedUrl" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
   </div>
 </template>
 
 <script>
   export default {
     name: "VideoModule",
-    data: {
-    	embed-url: "https://www.youtube.com/watch?v=C0DPdy98e4c"
+    data() {
+      return {
+    	   embedUrl: "https://www.youtube.com/embed/C0DPdy98e4c"
+      }
     }
   }
-
 </script>
