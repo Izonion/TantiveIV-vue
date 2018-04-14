58ca67aa60deb7d0ab781a86c6a279b764a97046
Marissa Angell <littlemissriss@gmail.com>
2018-04-13 18:36:05 -0600

    Create sign-in page template

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
+<div class="row justify-content-center">
+  <div id="container" class="card-sm-6">
+    <div class="card-body">
+    <form class="form-signin">
+      <div style="font-size:3em; color:#02577a">
+        <i class="fas fa-book"></i>
+      </div>
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-3">Don't have an account? <button class="btn btn-sm btn-secondary">Sign Up</button></p>
+      <p class="mt-3 mb-1 text-muted">&copy; 2018</p>
+    </form>
   </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -13,3 +35,59 @@
     }
   }
 </script>
+
+<style scoped>
+  body {
+    display: -ms-flexbox;
+    display: -webkit-box;
+    display: flex;
+    -ms-flex-align: center;
+    -ms-flex-pack: center;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    background-color: #f5f5f5;
+  }
+
+  .form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: 0 auto;
+  }
+  .form-signin .checkbox {
+    font-weight: 400;
+  }
+  .form-signin .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
+  }
+  .form-signin .form-control:focus {
+    z-index: 2;
+  }
+  .form-signin input[type="email"] {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+  .form-signin input[type="password"] {
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+  #container{
+    background-color: white;
+    margin: 2em;
+    border-radius: 10px;
+  }
+  .row {
+    width: 100%;
+  }
+
+</style>
